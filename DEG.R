ea7d7568c679511a8b2f09a49f1ee7d9ee0fc6f4
alvaroextremera10-dev <alvaroextremera10@gmail.com>
2026-06-10 18:05:31 +0200

    Update DEG.R

--- DEG.R
+++ DEG.R
@@ -1,10 +1,10 @@
-#En este script vamos a hacer el DEG para CTSD high y low
-#Como ya hemos visto la expresion de CTSD en el script anterior
-#ahora vamos a empezar definiendo CTSD high y low
-ctsd_exp <- FetchData(mieloide, vars="CTSD")
-threshold<- median(ctsd_exp$CTSD)
-mieloide$CTSD_group <- ifelse(ctsd_exp$CTSD >threshold, "high", "low")
-Idents(mieloide)<- "CTSD_group"
+#En este script vamos a hacer el DEG para Gen X high y low
+#Como ya hemos visto la expresion de Gen X en el script anterior
+#ahora vamos a empezar definiendo Gen X high y low
+genx_exp <- FetchData(mieloide, vars="GenX")
+threshold<- median(ctsd_exp$GenX)
+mieloide$GenX_group <- ifelse(ctsd_exp$GenX >threshold, "high", "low")
+Idents(mieloide)<- "GenX_group"
 deg<- FindMarkers(
   mieloide,
   ident.1 = "high",
@@ -25,8 +25,8 @@ ego <- enrichGO(
   ont = "BP"
 )
 dotplot(ego)
-saveRDS(deg, "deg_ctsd.rds")
-saveRDS(ego, "ego_ctsd.rds")
+saveRDS(deg, "deg_genx.rds")
+saveRDS(ego, "ego_genx.rds")
 #tenemos que guardar la tabla de genes
 genes_up_df <- data.frame(gene=genes_up)
 write.csv(genes_up_df, "genes_up.csv", row.names = FALSE)
@@ -34,4 +34,4 @@ saveRDS(genes_up_df, "genes_up")
 #vamos a ver los 10 mas sobreexpresados
 top_up <-rownames(deg[order(-deg$avg_log2FC), ])[1:10]
 View(top_up)
-write.csv(top_up, "top10_high_ctsd.csv")
+write.csv(top_up, "top10_high_genx.csv")
